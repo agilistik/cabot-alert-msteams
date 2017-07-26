e7698ca456aef5534e160dfd20cc462e648f5506
maxim.kur <maximkur@Maxim-Kurs-MacBook-Pro.local>
2017-07-26 13:36:03 -0700

    Initial check-in

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='cabot-alert-msteams',
+      version='0.0.1',
+      description='An msteams alert plugin for Cabot by Arachnys',
+      author='mk',
+      author_email='maxim.kur@exponential.com',
+      url='http://cabotapp.com',
+      packages=find_packages(),
+      download_url = 'https://github.com/agilistik/cabot-msteams/tarball/0.0.1',
+     )
